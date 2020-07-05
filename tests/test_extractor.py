c484d2ed2ff4afea6528e9eabada80e3318010cc
jsngn <mien.t.nguyen.22@dartmouth.edu>
2020-07-04 20:45:18 -0400

    No tests depend on connection/requests to Reddit

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -10,17 +10,46 @@ from statics import Statics
 
 
 @pytest.fixture
-def red():
+def lis():
+    """ Fixture of mock Listing instance to be used as default return of GET requests; for test functions that
+        don't require extracting subreddit or award count/freq """
+
+    return sentinel.Listing
+
+
+@pytest.fixture
+def red(lis):
     """ Fixture of mock Reddit instance """
 
+    red = sentinel.Reddit
+    red.get = MagicMock(return_value=lis)
     return sentinel.Reddit
+
+
+@pytest.fixture
+def stt():
+    """ Fixture of mock results w/ important info returned by GET requests; for test functions that
+        require extracting subreddit or award count/freq """
+
+    # Make 2 mock results to be returned from mock Reddit.get(); actual Listing objects would have more attributes
+    res_1 = MagicMock()
+    res_1.subreddit = "memes"
+    res_1.permalink = "/r/memes"  # Actual link returned would point to a specific comment, not this
+    res_1.all_awardings = [{"name": "Gold", "count": 7}, {"name": "Silver", "count": 17}]
+
+    res_2 = MagicMock()
+    res_2.subreddit = "redditdev"
+    res_2.permalink = "/r/redditdev"
+    res_2.all_awardings = [{"name": "Gold", "count": 1}, {"name": "Silver", "count": 12}]
+
+    return [res_1, res_2]
 
 
 @pytest.fixture
 def usr():
     """ Fixture of string for username to get stats of """
 
-    return "swinii"  # FOR SMOOTHEST TEST DEMO REPLACE WITH PUBLIC UPVOTES AND AWARDS ON SUBMISSIONS
+    return "swinii"
 
 
 @pytest.fixture
@@ -49,60 +78,63 @@ def test_extractor_constructor(red, ext, usr):
     assert ext.mode_action[Statics.AWARD_FREQ_KW] == ext.get_award_freqs_from_list
 
 
-def test_get_calls(red, ext):
+def test_get_calls(red, ext, lis):
     """ Test that all GET requests return expected value """
 
-    listing = sentinel.Listing
-    red.get = MagicMock(return_value=listing)
-
-    assert ext.get_upvotes_stats() is listing and red.get.called
-    assert ext.get_submissions_stats() is listing and red.get.called
-    assert ext.get_comments_stats() is listing and red.get.called
+    assert ext.get_upvotes_stats() is lis and red.get.called
+    assert ext.get_submissions_stats() is lis and red.get.called
+    assert ext.get_comments_stats() is lis and red.get.called
 
 
-def test_get_from_list(red, ext):
-    """ Call function to test type of dictionary response and its key-value pair types, also tests that only 1 of the
-        functions return URL as keys """
+def test_get_from_list(red, ext, stt):
+    """ Call function to test type of dictionary response, its key-value pair types, and length, also tests that only
+        1 of the functions return URL as keys """
+
+    red.get.return_value = stt
 
     res = ext.get_subreddit_count_from_list(ext.get_upvotes_stats())
-    check_get_from_list_content(res)
+    check_get_from_list_content(res, stt)
     for url in res:
         assert urlparse(url).netloc == ""  # not URL
 
     res = ext.get_award_freqs_from_list(ext.get_submissions_stats())
-    check_get_from_list_content(res)
+    check_get_from_list_content(res, stt)
     for url in res:
         assert urlparse(url).netloc == ""
 
     res = ext.get_award_count_from_list(ext.get_submissions_stats())
-    check_get_from_list_content(res)
+    check_get_from_list_content(res, stt)
     for url in res:
         assert urlparse(url).netloc != ""  # is URL
 
 
-def check_get_from_list_content(res):
-    """ Test that each get_x_from_list() return value is right type, including keys and values """
+def check_get_from_list_content(res, stt):
+    """ Test that each get_x_from_list() return value is right type, including keys and values, and length """
 
     assert type(res) is dict
+    assert len(res) == len(stt)
+
     if res.keys():
         assert set(map(type, res.keys())) == {str}
     if res.values():
         assert set(map(type, res.values())) == {int}
 
 
-def test_extract(ext):
+def test_extract(red, ext, stt):
     """ Test valid runs of extract() """
 
-    check_extract_content(ext, Statics.UPVOTES_KW, Statics.SUBREDDIT_KW)
-    check_extract_content(ext, Statics.COMMENTS_KW, Statics.SUBREDDIT_KW)
-    check_extract_content(ext, Statics.COMMENTS_KW, Statics.AWARD_FREQ_KW)
-    check_extract_content(ext, Statics.COMMENTS_KW, Statics.AWARD_COUNT_KW)
-    check_extract_content(ext, Statics.SUBMISSIONS_KW, Statics.SUBREDDIT_KW)
-    check_extract_content(ext, Statics.SUBMISSIONS_KW, Statics.AWARD_FREQ_KW)
-    check_extract_content(ext, Statics.SUBMISSIONS_KW, Statics.AWARD_COUNT_KW)
+    red.get.return_value = stt
+
+    check_extract_content(ext, stt, Statics.UPVOTES_KW, Statics.SUBREDDIT_KW)
+    check_extract_content(ext, stt, Statics.COMMENTS_KW, Statics.SUBREDDIT_KW)
+    check_extract_content(ext, stt, Statics.COMMENTS_KW, Statics.AWARD_FREQ_KW)
+    check_extract_content(ext, stt, Statics.COMMENTS_KW, Statics.AWARD_COUNT_KW)
+    check_extract_content(ext, stt, Statics.SUBMISSIONS_KW, Statics.SUBREDDIT_KW)
+    check_extract_content(ext, stt, Statics.SUBMISSIONS_KW, Statics.AWARD_FREQ_KW)
+    check_extract_content(ext, stt, Statics.SUBMISSIONS_KW, Statics.AWARD_COUNT_KW)
 
 
-def check_extract_content(ext, activity, mode):
+def check_extract_content(ext, stt, activity, mode):
     """ Make sure no error returned for a valid extract() call (NOT including the format checking that should've
         occurred in bot.py) and correct result for activity & mode types are returned """
 
@@ -110,14 +142,15 @@ def check_extract_content(ext, activity, mode):
     assert type(res) is dict  # Make sure something is returned (not NoneType) and it's a dict
     assert Statics.EXCEPTION_KW not in res
     assert activity + "_" + mode in res
+    assert type(res[activity + "_" + mode]) is dict
+    assert len(res[activity + "_" + mode]) == len(stt)  # All (mock) results are present
 
 
-def test_extract_exception(ext):
-    """ Test that an exception is raised, and that result dictionary indicates error """
+def test_extract_exception(red, ext):
+    """ Test when an exception is raised, and that result dictionary indicates error, not normal result """
 
-    ext.reddit = sentinel.Reddit  # Not actual praw.Reddit instance, will throw some exception
-    with pytest.raises(Exception):
-        res = ext.extract(Statics.COMMENTS_KW, Statics.AWARD_COUNT_KW)
-        assert type(res) is dict  # Make sure something is returned (not NoneType) and it's a dict
-        assert Statics.EXCEPTION_KW in res
-        assert Statics.COMMENTS_KW + "_" + Statics.AWARD_COUNT_KW in res
+    red.get.side_effect = Exception()
+    res = ext.extract(Statics.COMMENTS_KW, Statics.AWARD_COUNT_KW)
+    assert type(res) is dict  # Make sure something is returned (not NoneType) and it's a dict
+    assert Statics.EXCEPTION_KW in res
+    assert Statics.COMMENTS_KW + "_" + Statics.AWARD_COUNT_KW not in res
